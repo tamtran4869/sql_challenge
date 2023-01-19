d581d92da0fc6a501105f96c1a724a767c618b20
tamtran4869 <114192113+tamtran4869@users.noreply.github.com>
2023-01-19 00:19:12 +0000

    Update clique_bait.sql

--- Clique_bait/clique_bait.sql
+++ Clique_bait/clique_bait.sql
@@ -20,10 +20,7 @@ FROM events
 GROUP BY 1;
 
 -- Question 4
-SELECT
-	*
-FROM events
-WHERE event_type IS NULL;
+
 
 SELECT
 	ei.event_name,
@@ -48,28 +45,28 @@ ORDER BY 2 DESC;
 --Question 6:
 
 WITH view_checkout_id AS (
-SELECT -- get viewing checkout visits
-	visit_id,
-	event_time AS view_checkout_time
-FROM events e
-LEFT JOIN page_hierarchy ph
-	ON e.page_id = ph.page_id
-WHERE 
-	ph.page_name = 'Checkout'
-),
-
+	SELECT -- get viewing checkout visits
+		visit_id,
+		event_time AS view_checkout_time
+	FROM events e
+	LEFT JOIN page_hierarchy ph
+		ON e.page_id = ph.page_id
+	WHERE 
+		ph.page_name = 'Checkout'
+	),
+	
 purchase_visits AS (
-SELECT -- get purchase visits from viewing checkout visit
-	(SELECT COUNT(visit_id) FROM view_checkout_id) AS checkout_visits,
-	COUNT(e.visit_id) AS purchase_visits
-FROM events e
-RIGHT JOIN view_checkout_id vci
-	ON e.visit_id = vci.visit_id
-	AND e.event_time > vci.view_checkout_time
-WHERE 
-	e.event_type = 3
-)
-
+	SELECT -- get purchase visits from viewing checkout visit
+		(SELECT COUNT(visit_id) FROM view_checkout_id) AS checkout_visits,
+		COUNT(e.visit_id) AS purchase_visits
+	FROM events e
+	RIGHT JOIN view_checkout_id vci
+		ON e.visit_id = vci.visit_id
+		AND e.event_time > vci.view_checkout_time
+	WHERE 
+		e.event_type = 3
+	)
+	
 SELECT
 	checkout_visits,
 	purchase_visits,
@@ -105,65 +102,65 @@ GROUP BY 1;
 
 -- Question 9:
 WITH cart_add_id AS (
-SELECT -- get cart adding visits
-	ph.page_name AS product,
-	e.visit_id,
-	e.event_time AS add_cart_time
-FROM events e
-LEFT JOIN page_hierarchy ph
-	ON e.page_id = ph.page_id
-WHERE e.event_type = 2
-),
+	SELECT -- get cart adding visits
+		ph.page_name AS product,
+		e.visit_id,
+		e.event_time AS add_cart_time
+	FROM events e
+	LEFT JOIN page_hierarchy ph
+		ON e.page_id = ph.page_id
+	WHERE e.event_type = 2
+	),
 
 purchase_visits AS (
-SELECT -- get purchase visits from add product to cart visit
-	cai.product,
-	COUNT(e.visit_id) AS purchase_visits
-FROM events e
-RIGHT JOIN cart_add_id cai
-	ON e.visit_id = cai.visit_id
-	AND e.event_time > cai.add_cart_time
-WHERE 
-	e.event_type = 3
-GROUP BY 1
-)
+	SELECT -- get purchase visits from add product to cart visit
+		cai.product,
+		COUNT(e.visit_id) AS purchase_visits
+	FROM events e
+	RIGHT JOIN cart_add_id cai
+		ON e.visit_id = cai.visit_id
+		AND e.event_time > cai.add_cart_time
+	WHERE 
+		e.event_type = 3
+	GROUP BY 1
+	)
 SELECT *
 FROM purchase_visits
 ORDER BY 2 DESC
 LIMIT 3;
 
--- PART 3
---Create product_sales table
+--PART 3: Product Funnel Analysis
+--Question 1: Create product_sales table
 DROP TABLE IF EXISTS product_sales;
 CREATE TABLE product_sales AS 
 WITH funnel_flag AS (
-SELECT -- flag funnel steps of each product
-	product_id,
-	product,
-	visit_id,
-	MAX(view_product) AS view_madeit,
-	MAX(add_cart) AS add_madeit,
-	MAX(purchase) AS purchase_madeit
-FROM 	(
-	SELECT -- convert event_type into column
-		ph.product_id,
-		ph.page_name AS product,
-		e.visit_id,
-		CASE WHEN e.event_type = 1 THEN 1 ELSE 0 END AS view_product,
-		CASE WHEN e.event_type = 2 THEN 1 ELSE 0 END AS add_cart,
-		CASE WHEN 
-			e.event_type = 2 AND 
-			e.visit_id IN (SELECT DISTINCT visit_id FROM events WHERE event_type =3)-- the subquery to get purchase visits
-			THEN 1 
-			ELSE 0 
-			END AS purchase 
-	FROM events e
-	LEFT JOIN page_hierarchy ph
-		ON e.page_id = ph.page_id
-	WHERE ph.product_id IS NOT NULL
-	) AS mark_step
-GROUP BY 1,2,3
-)
+	SELECT -- flag funnel steps of each product
+		product_id,
+		product,
+		visit_id,
+		MAX(view_product) AS view_madeit,
+		MAX(add_cart) AS add_madeit,
+		MAX(purchase) AS purchase_madeit
+	FROM 	(
+		SELECT -- convert event_type into column
+			ph.product_id,
+			ph.page_name AS product,
+			e.visit_id,
+			CASE WHEN e.event_type = 1 THEN 1 ELSE 0 END AS view_product,
+			CASE WHEN e.event_type = 2 THEN 1 ELSE 0 END AS add_cart,
+			CASE WHEN 
+				e.event_type = 2 AND 
+				e.visit_id IN (SELECT DISTINCT visit_id FROM events WHERE event_type =3)-- the subquery to get purchase visits
+				THEN 1 
+				ELSE 0 
+				END AS purchase 
+		FROM events e
+		LEFT JOIN page_hierarchy ph
+			ON e.page_id = ph.page_id
+		WHERE ph.product_id IS NOT NULL
+		) AS mark_step
+	GROUP BY 1,2,3
+	)
 SELECT -- compute count by products
 	product_id,
 	product,
@@ -176,7 +173,7 @@ GROUP BY 1
 ORDER BY 1;
 
 
--- Create category table 
+-- Question 2: Create category table 
 DROP TABLE IF EXISTS category_sales;
 CREATE TABLE category_sales AS
 SELECT 
@@ -189,27 +186,29 @@ LEFT JOIN page_hierarchy ph
 	ON ps.product_id = ph.product_id
 GROUP BY 1;
 
---PART 3: Product Funnel Analysis
--- Question 1: 
+-- Question 3: 
 SELECT 
+	'Most views' AS top,
 	product,
 	views,
 	cart_adds,
 	purchases
 FROM product_sales
 WHERE 
-	views = (SELECT MAX(views) FROM product_sales);
-	
+	views = (SELECT MAX(views) FROM product_sales)
+UNION ALL	
 SELECT 
+	'Most cart adds' AS top,
 	product,
 	views,
 	cart_adds,
 	purchases
 FROM product_sales
 WHERE 
-	cart_adds = (SELECT MAX(cart_adds) FROM product_sales);
-
+	cart_adds = (SELECT MAX(cart_adds) FROM product_sales)
+UNION ALL
 SELECT 
+	'Most purchases' AS top,
 	product,
 	views,
 	cart_adds,
@@ -218,7 +217,7 @@ FROM product_sales
 WHERE 
 	purchases = (SELECT MAX(purchases) FROM product_sales);
 
---Question 2:
+--Question 4:
 SELECT 
 	product_id,
 	product,
@@ -227,7 +226,7 @@ SELECT
 FROM product_sales
 ORDER BY 4 DESC;
 
--- Question 3:
+-- Question 5:
 SELECT 
 	product_id,
 	product,
@@ -235,29 +234,29 @@ SELECT
 FROM product_sales
 ORDER BY 3 DESC;
 
--- Question 4&5
+-- Question 6&7
 SELECT 
-	AVG(cart_adds/views) AS view_to_add_ctr,
-	AVG(purchases/cart_adds) AS add_to_purchase_ctr
+	AVG(cart_adds/views) AS view_to_add_cvr,
+	AVG(purchases/cart_adds) AS add_to_purchase_cvr
 FROM product_sales;
 
--- Part 4
--- Create campaign table
+-- Part 4: Campaigns Analysis
+-- Question 1: Create campaign table
 DROP TABLE IF EXISTS campaign_performance;
 CREATE TABLE campaign_performance AS
 WITH cart_products AS (
-SELECT -- get info of products added to the cart
-	e.visit_id,
-	e.sequence_number,
-	GROUP_CONCAT(ph.page_name SEPARATOR ', ') AS cart_products
-FROM events e
-LEFT JOIN page_hierarchy ph
-	ON e.page_id = ph.page_id
-WHERE 
-	e.event_type = 2
-GROUP BY 1
-ORDER BY 1,2
-)
+	SELECT -- get info of products added to the cart
+		e.visit_id,
+		e.sequence_number,
+		GROUP_CONCAT(ph.page_name SEPARATOR ', ') AS cart_products
+	FROM events e
+	LEFT JOIN page_hierarchy ph
+		ON e.page_id = ph.page_id
+	WHERE 
+		e.event_type = 2
+	GROUP BY 1
+	ORDER BY 1,2
+	)
 SELECT -- compute metrics
 	u.user_id,
 	e.visit_id,
@@ -278,8 +277,7 @@ LEFT JOIN cart_products cp
 	ON e.visit_id = cp.visit_id
 GROUP BY 1,2;
 
---PART 4: Campaign Analysis
--- Question 1:
+--Question 2:
 SELECT -- compute pct 
 	impressions,
 	user_count,
@@ -305,7 +303,7 @@ FROM	(
 	) 
 	AS info;
 
--- Question 2:
+-- Question 3 & 4:
 SELECT -- compute pct 
 	impressions,
 	clicks,
@@ -333,7 +331,7 @@ FROM	(
 	) 
 	AS info;
 
--- Question 3:
+-- Question 5 :
 SELECT -- compute pct 
 	campaign_name,
 	user_count,
@@ -359,6 +357,3 @@ FROM	(
 	GROUP BY 1
 	) 
 	AS info;
-	
-
-
